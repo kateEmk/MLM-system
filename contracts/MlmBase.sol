c6d72c5d7332a80b7ab440d287aa22098a05fbbb
katsiarynaErmachenok <e.ermachenok@icloud.com>
2022-07-22 18:41:43 +0300

    fix

--- contracts/MlmBase.sol
+++ contracts/MlmBase.sol
@@ -2,19 +2,65 @@
 pragma solidity ^0.8.12;
 
 contract MlmSystem {
-    
-    uint8 public constant MINIMUM_ENTER = 20 wei;
-    uint256[10] public levelInvestments = [0.005*1e18, 0.01*1e18, 0.02*1e18, 0.05*1e18, 0.1*1e18, 0.2*1e18, 0.5*1e18, 1e18, 2*1e18, 5*1e18];
-    uint256[10] public levelComissions = [10, 7, 5, 2, 1, 1, 1, 1, 1, 1]; // .../10 - per cent
 
-    mapping (address => address[]) partnersUsers;       // address of directPartner -> users who entered with his referalLink
-    mapping (address => uint256) accountBalance;        // user address - balance of his account
-    mapping (address => address) referalUser;           // user - referal (who invited user)
+    uint64 MINIMUM_ENTER;
+    uint256[10] levelInvestments;
+    uint256[10] levelComissions;
+
+    mapping (address => address[]) public partnersUsers;       // address of directPartner -> users who entered with his referalLink //referals
+    mapping (address => uint256) public accountBalance;        // user address - balance of his account
+    mapping (address => address) public referalUser;           // user - referal (who invited user) //referrers
+
+    constructor() {
+        MINIMUM_ENTER = 5000000000000000;
+        levelInvestments = [0.005*1e18, 0.01*1e18, 0.02*1e18, 0.05*1e18, 0.1*1e18, 0.2*1e18, 0.5*1e18, 1e18, 2*1e18, 5*1e18];
+        levelComissions = [10, 7, 5, 2, 1, 1, 1, 1, 1, 1]; // .../10 - per cent
+    }
 
     receive() payable external {}
     fallback() payable external {}
 
-    function enter(address _referalLink) private {   
+    function investing(uint256 _amountInvest) public payable {
+        require(_amountInvest >= MINIMUM_ENTER, "Didn't send enough");
+        uint comissionToContract = _amountInvest / 20;
+        uint valueToUser = _amountInvest - comissionToContract;
+
+        (bool successToUser, ) = payable(msg.sender).call{value: valueToUser}("");
+        require(successToUser, "Transfer failed");
+        accountBalance[msg.sender] += valueToUser;
+
+        (bool sucessToContract, ) = payable(address(this)).call{value: comissionToContract}("");
+        require(sucessToContract, "Transfer failed");
+    }
+
+    // money to user from his account
+    function withdraw() public returns(bool) {
+        uint userBalance = accountBalance[msg.sender];
+        require(userBalance > 0, "Your current balance is 0");
+
+        address current = msg.sender;
+        uint counterDepth = 0;
+        uint comission = 0;
+    
+        for (uint i = 0; i<10; i++) {
+            while(current != address(0)) {
+                counterDepth++;
+                current = referalUser[msg.sender];
+                comission = userBalance * levelComissions[_getLevel(current)] / 10;
+                (bool success, ) = payable(current).call{value: comission}("");
+                require(success, "Transfer failed");
+                userBalance -= comission;
+            }
+        }
+
+        (bool successUser, ) = payable(msg.sender).call{value: userBalance}("");
+        require(successUser, "Transfer failed");
+        accountBalance[msg.sender] = 0;
+
+        return true;
+    }
+
+    function registration(address _referalLink) private {   
         if(_referalLink != address(0)) {
             partnersUsers[_referalLink].push(msg.sender);   
             accountBalance[msg.sender] = 0;      
@@ -24,67 +70,22 @@ contract MlmSystem {
         }
     }
 
-    function investing(uint256 _amountInvest) public payable {
-        require(_amountInvest >= MINIMUM_ENTER, "Didn't send enough");
-        payable(address(msg.sender)).transfer(_amountInvest - (_amountInvest / 20));
-        payable(address(this)).transfer(_amountInvest / 20);
-        accountBalance[msg.sender] += _amountInvest - (_amountInvest / 20);
-    }
-
-    function getLevel(address _userAddress) private view returns(uint256) {
+    function _getLevel(address _userAddress) private view returns(uint256) {
         for(uint i = 0; i<levelInvestments.length; i++) {   
-            if (accountBalance[_userAddress] == levelInvestments[i]) {
+            if (accountBalance[_userAddress] <= levelInvestments[i] && accountBalance[_userAddress] >= levelInvestments[i-1]) {
                 return i+1;
             }
         }
     }
 
     // user can see the amount of direct partners and their level
-    function directPartnersInfo() private view returns(address, uint256) {
-        address[] memory _addressDirectPartner = partnersUsers[msg.sender];
-        for(uint i = 0; i<_addressDirectPartner.length; i++){
-            return (_addressDirectPartner[i], getLevel(_addressDirectPartner[i]));
+    function _directPartnersInfo() private view returns(uint, uint[] memory) {
+        uint[] memory partnersLevel;
+        address[] memory partnersAddresses = partnersUsers[msg.sender];
+        for(uint i = 0; i<partnersUsers[msg.sender].length; i++){
+            partnersLevel[i] = _getLevel(partnersAddresses[i]);
         }
-    }
-
-    // send money to user account
-    function send(uint256 _amount) public payable returns(bool) {
-        (bool success, ) = payable(msg.sender).call{value: _amount}("");
-        require(success, "Transfer failed");
-        accountBalance[msg.sender] = 0;
-        return true;
-    }
-
-    function comissionSend(address _partnerAddress) public payable {  
-        uint counterDepth = 0;
-        address current = msg.sender;
-        while(current!=address(0)) {
-            if (referalUser[current] != address(0)){
-                counterDepth ++;
-                current = referalUser[current];
-            } else { current = address(0); }
-        }
-
-        require(getLevel(msg.sender) >= counterDepth, "Comission to user won't be send");
-
-        // direct partner withdraw money - % of money will go to user's account
-        uint256 amount = accountBalance[_partnerAddress];
-        (bool success, ) = payable(_partnerAddress).call{value: amount}("");
-        require(success, "Transfer failed");
-        accountBalance[_partnerAddress] = 0;
-
-        send(amount * levelComissions[getLevel(msg.sender)] / 10);
-    }
-
-    // money to user from his account
-    function withdraw() public returns(bool) {
-        require( accountBalance[msg.sender] > 0, "Your current balance is 0");
-        
-        (bool success, ) = payable(msg.sender).call{value: accountBalance[msg.sender]}("");
-        require(success, "Transfer failed");
-        accountBalance[msg.sender] = 0;
-
-        return true;
+        return(partnersUsers[msg.sender].length, partnersLevel);
     }
 
 }
