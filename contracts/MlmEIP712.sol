68ad07d8772ced044c13f3a99407d5595f638b0f
katsiarynaErmachenok <e.ermachenok@icloud.com>
2022-08-09 11:59:31 +0300

    edit

--- contracts/MlmEIP712.sol
+++ contracts/MlmEIP712.sol
@@ -8,11 +8,10 @@ contract MlmEIP712 is EIP712 {
     using ECDSA for bytes32;
 
     struct Message {
-        string name;
-        address from;
-        address to;
-        uint256 amount;
-        string contents;
+        address from;           // Externally-owned account (EOA) making the request.
+        address to;             // Destination address, normally a smart contract.
+        uint256 value;
+        bytes data;            // (Call)data to be sent to the destination.
     }
 
     struct Domain {
@@ -23,16 +22,39 @@ contract MlmEIP712 is EIP712 {
         bytes32 salt;
     }
 
-    bytes32 private constant hash = keccak256("Message(address from, address to, string contents)");
+    bytes32 private constant message_HASH = keccak256("Message(address from, address to, uint256 value, bytes data)");
+    bytes32 private constant domain_HASH = keccak256("Domain(string name, string version, uint256 chainId, address verifyingContract, bytes32 salt)");
 
     constructor() EIP712("MlmEIP712", "0.0.1") {}
 
-    function verify() public view returns(bool) {
-
+    // Returns the domain separator used in the encoding of the signature for `execute`, as defined by {EIP712}.
+    function DOMAIN_SEPARATOR() external view returns (bytes32) {
+        return _domainSeparatorV4();
     }
 
-    function execute() public payable returns(bool, bytes memory) {
+    function veryfyingContract() public view returns(address) {
+        return address(this);
+    }
 
+    function verify(Message calldata req, bytes calldata signature) public view returns(bool) {
+        address signer = _hashTypedDataV4(keccak256(abi.encode(
+            message_HASH,
+            req.from,
+            req.to,
+            req.value,
+            keccak256(req.data)
+        ))).recover(signature);
+        return signer == req.from;
+    }
+
+    function execute(Message calldata req, bytes calldata signature) public payable returns(bool, bytes memory) {
+        // require(verify(req, signature), "Signature does not match request");
+
+        // (bool success, bytes memory returndata) = req.to.call{value: req.value}(
+        //     abi.encodePacked(req.data, req.from)
+        // );
+
+        // return (success, returndata);
     }
 
 }
